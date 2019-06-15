e1ef8a95ea774f3e16ca4a91bc46c00c3f9177d2
Keith Smith <ksmith51@mail.usf.edu>
2019-06-15 16:36:29 -0400

    Registering a user now adds a bunch of personal information to the      real-time database

--- Fitness_App/app/src/main/java/com/jando/fitness_app/MainActivity.java
+++ Fitness_App/app/src/main/java/com/jando/fitness_app/MainActivity.java
@@ -16,19 +16,35 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.jando.fitness_app.Model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonRegister;
+
+    private EditText editTextFirstName;
+    private EditText editTextLastName;
+    private EditText editTextAge;
+    private EditText editTextWeight;
+    private EditText editTextUsername;
     private EditText editTextEmail;
     private EditText editTextPassword;
+
     private TextView textViewSignin;
+
     private ProgressDialog progressDialog;
     private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference usersRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        usersRef = database.getReference("Users");
 
         if (firebaseAuth.getCurrentUser() != null) {
             finish();
@@ -46,8 +64,15 @@ public class MainActivity extends AppCompatActivity {
 
         //views
         buttonRegister = findViewById(R.id.buttonSignin);
+
+        editTextFirstName = findViewById(R.id.editTextFirstName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        editTextAge = findViewById(R.id.editTextAge);
+        editTextWeight = findViewById(R.id.editTextWeight);
+        editTextUsername = findViewById(R.id.editTextUsername);
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextInputPassword);
+
         textViewSignin = findViewById(R.id.textViewSignUp);
 
         //closes keyboard when password editText is entered
@@ -67,6 +92,36 @@ public class MainActivity extends AppCompatActivity {
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Gets all information to be stored in database
+                final User user = new User(editTextUsername.getText().toString(),
+                        editTextPassword.getText().toString(),
+                        editTextEmail.getText().toString(),
+                        editTextFirstName.getText().toString(),
+                        editTextLastName.getText().toString(),
+                        editTextAge.getText().toString(),
+                        editTextWeight.getText().toString());
+
+                //Stores User object inside database using email as label
+                usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.child(user.getUsername()).exists()){
+                            Toast.makeText(MainActivity.this,
+                                    "Email Already Registered!",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            usersRef.child(user.getUsername()).setValue(user);
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(MainActivity.this,
+                                "Database Error",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                
+                //Registers user in FireBase authorization
                 registerUser();
             }
         });
@@ -81,10 +136,42 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /** Function that is called to add new user to FireBase Authorization **/
     private void registerUser() {
+        String firstname = editTextFirstName.getText().toString().trim();
+        String lastname = editTextLastName.getText().toString().trim();
+        String age = editTextAge.getText().toString().trim();
+        String weight = editTextWeight.getText().toString().trim();
+        String username = editTextUsername.getText().toString().trim();
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
 
+        //Errors if any field is incorrectly filled out
+        if (TextUtils.isEmpty(firstname)) {
+            editTextFirstName.setError("First Name Required");
+            editTextFirstName.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(lastname)) {
+            editTextLastName.setError("Last Name Required");
+            editTextLastName.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(age)) {
+            editTextAge.setError("Age Required");
+            editTextAge.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(weight)) {
+            editTextWeight.setError("Weight Required");
+            editTextWeight.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(username)) {
+            editTextUsername.setError("Username Required");
+            editTextUsername.requestFocus();
+            return;
+        }
         if (TextUtils.isEmpty(email)) {
             editTextEmail.setError("Email Required");
             editTextEmail.requestFocus();
@@ -101,9 +188,9 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
+        //Registers user with email and password
         progressDialog.setMessage("Registering Please wait");
         progressDialog.show();
-
         firebaseAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
---
+++ Fitness_App/app/src/main/java/com/jando/fitness_app/Model/User.java
@@ -0,0 +1,64 @@
+package com.jando.fitness_app.Model;
+
+public class User {
+
+    private String username, password, email, firstname, lastname, age, weight;
+
+    public User() {
+
+    }
+
+    public User(String username, String password, String email, String firstname,
+                String lastname, String age, String weight) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String input) {
+        this.username = input;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String input) {
+        this.password = input;
+    }
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String input) {
+        this.firstname = input;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String input) {
+        this.lastname = input;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String input) {
+        this.email = input;
+    }
+    public String getAge() {
+        return age;
+    }
+    public void setAge(String input) {
+            this.age = input;
+    }
+    public String getWeight() {
+        return weight;
+    }
+    public void setWeight(String input) {
+            this.weight = input;
+    }
+}
