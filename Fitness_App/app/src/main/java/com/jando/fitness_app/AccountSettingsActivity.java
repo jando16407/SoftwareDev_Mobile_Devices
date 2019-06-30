9816eb5900960dd5ecc66f5fbc9cfa753237b05d
Juri Ando <jando16407c@gmail.com>
2019-06-30 17:55:53 -0400

    Account Settings Page completed.
    User can change email and password

--- Fitness_App/app/src/main/java/com/jando/fitness_app/AccountSettingsActivity.java
+++ Fitness_App/app/src/main/java/com/jando/fitness_app/AccountSettingsActivity.java
@@ -1,12 +1,15 @@
 package com.jando.fitness_app;
 
 import android.accounts.Account;
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,21 +34,14 @@ public class AccountSettingsActivity extends AppCompatActivity {
     private GoogleSignInClient mGoogleSignInClient;
     private GoogleApiClient mGoogleApiClient;
     private FirebaseAuth firebaseAuth;
-    private TextView textViewUserEmail;
+    private TextView textViewUserEmail1;
+    private TextView textViewUserEmail2;
     private TextView textViewUserPassword1;
     private TextView textViewUserPassword2;
-    private TextView textViewUserAge;
-    private TextView textViewUserWeight;
-    private TextView textViewUserFirstName;
-    private TextView textViewUserLastName;
     //private FirebaseAuth firebaseAuth;
     private FirebaseDatabase database;
     private DatabaseReference usersRef;
     private Button buttonEmail;
-    private Button buttonAge;
-    private Button buttonWeight;
-    private Button buttonFirstName;
-    private Button buttonLastName;
     private Button buttonPassword;
     final User userInfo = new User("", "", "", "", "");
     FirebaseUser user = null;
@@ -55,15 +51,6 @@ public class AccountSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
-
-        /*
-        //Gets Google info so it can log out
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("434381172772-nab3ojvfhn78s3s6en73mdbmg9pk30ak.apps.googleusercontent.com")
-                .requestEmail()
-                .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-*/
 
         //firebase info to log out
         firebaseAuth = FirebaseAuth.getInstance();
@@ -76,25 +63,14 @@ public class AccountSettingsActivity extends AppCompatActivity {
 
         user = firebaseAuth.getCurrentUser();
 
-        //textViewUserName = findViewById(R.id.editText2);
-        //textViewUserName.setText(user.getu);
-        //firebaseAuth = FirebaseAuth.getInstance();
-        textViewUserEmail = findViewById(R.id.editText4);
-        textViewUserEmail.setText(user.getEmail());
+        textViewUserEmail1 = findViewById(R.id.editText4);
+        textViewUserEmail1.setText(user.getEmail());
+        textViewUserEmail2 = findViewById(R.id.editText44);
+        textViewUserEmail2.setText(user.getEmail());
         textViewUserPassword1 = findViewById(R.id.editText6);
-        //textViewUserPassword1.setText("Type password to update");
         textViewUserPassword2 = findViewById(R.id.editText66);
-        //textViewUserPassword2.setText("Type password to update");
-        textViewUserAge = findViewById(R.id.editText8);
-        textViewUserWeight = findViewById(R.id.editText10);
-        textViewUserFirstName = findViewById(R.id.editText12);
-        textViewUserLastName = findViewById(R.id.editText16);
         buttonEmail = findViewById(R.id.button1);
         buttonPassword = findViewById(R.id.button2);
-        buttonAge = findViewById(R.id.button3);
-        buttonWeight = findViewById(R.id.button4);
-        buttonFirstName = findViewById(R.id.button5);
-        buttonLastName = findViewById(R.id.button6);
 
 
         database = FirebaseDatabase.getInstance();
@@ -102,62 +78,113 @@ public class AccountSettingsActivity extends AppCompatActivity {
 
         displayUserInfo();
 
-        String firstname = textViewUserFirstName.getText().toString().trim();
-        String lastname = textViewUserLastName.getText().toString().trim();
-        String age = textViewUserAge.getText().toString().trim();
-        String weight = textViewUserWeight.getText().toString().trim();
-        //String email;
-        String password1 = textViewUserPassword1.getText().toString().trim();
-        String password2 = textViewUserPassword2.getText().toString().trim();
-
-
-
         /** Update email address */
         buttonEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                 final String email = textViewUserEmail.getText().toString().trim();
-                if (TextUtils.isEmpty(email)) {
-                    textViewUserEmail.setError("Please type the email to change");
-                    textViewUserEmail.requestFocus();
+                final String email1 = textViewUserEmail1.getText().toString().trim();
+                final String email2 = textViewUserEmail2.getText().toString().trim();
+                if(!email1.equals(email2)){
+                    textViewUserEmail1.setError("Emails does not match");
+                    textViewUserEmail1.requestFocus();
+                    textViewUserEmail2.setError("Emails does not match");
+                    textViewUserEmail2.requestFocus();
                     return;
                 }
-                else if(userInfo.getEmail().equals(email)){
-                    textViewUserEmail.setError("This is your current email address");
-                    textViewUserEmail.requestFocus();
+                if (TextUtils.isEmpty(email1)) {
+                    textViewUserEmail1.setError("Please type the email to change");
+                    textViewUserEmail1.requestFocus();
+                    return;
                 }
-                usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if(userEmailExists(dataSnapshot, email)){
-                            textViewUserEmail.setError("This email address is already registered");
-                            textViewUserEmail.requestFocus();
-                        }
-                        else{
-                            userInfo.setEmail(email);
-                            final FirebaseUser f_user = firebaseAuth.getInstance().getCurrentUser();
-                           // AuthCredential credential = EmailAuthProvider
-                             //       .getCredential(f_user.getEmail(), f_user.getPasswor);
-                            f_user.updateEmail(email).addOnCompleteListener(new OnCompleteListener<Void>(){
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()) {
-                                        Toast.makeText(AccountSettingsActivity.this, "User email address updated",Toast.LENGTH_SHORT).show();
-                                        usersRef.child(f_user.getUid()).setValue(userInfo);
-                                    }
-                                    else{
-                                        Toast.makeText(AccountSettingsActivity.this, "User email update Failed",Toast.LENGTH_SHORT).show();
-                                    }
+                if (TextUtils.isEmpty(email2)) {
+                    textViewUserEmail2.setError("Please type the email to confirm new address");
+                    textViewUserEmail2.requestFocus();
+                    return;
+                }
+                if(userInfo.getEmail().equals(email1)){
+                    textViewUserEmail1.setError("This is your current email address");
+                    textViewUserEmail1.requestFocus();
+                    return;
+                }
+                if(userInfo.getEmail().equals(email2)){
+                    textViewUserEmail2.setError("This is your current email address");
+                    textViewUserEmail2.requestFocus();
+                    return;
+                }
+                final FirebaseUser f_user = firebaseAuth.getInstance().getCurrentUser();
+                f_user.updateEmail(email1)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(AccountSettingsActivity.this,
+                                            "User email is updated",Toast.LENGTH_SHORT).show();
                                 }
-                            });
-                        }
-                    }
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                            }
+                        });
+            }
+        });
 
-                    }
-                });
+        /** Update password */
+        buttonPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String pass1 = textViewUserPassword1.getText().toString().trim();
+                final String pass2 = textViewUserPassword2.getText().toString().trim();
+                if(!pass1.equals(pass2)){
+                    textViewUserPassword1.setError("Password does not match");
+                    textViewUserPassword1.requestFocus();
+                    textViewUserPassword2.setError("Password does not match");
+                    textViewUserPassword2.requestFocus();
+                    return;
+                }
+                else if (TextUtils.isEmpty(pass1)) {
+                    textViewUserPassword1.setError("Please type the password to change");
+                    textViewUserPassword1.requestFocus();
+                    return;
+                }
+                else if (TextUtils.isEmpty(pass2)) {
+                    textViewUserPassword2.setError("Please type the password to confirm");
+                    textViewUserPassword2.requestFocus();
+                    return;
+                }
+                final FirebaseUser f_user = firebaseAuth.getInstance().getCurrentUser();
+                f_user.updatePassword(pass1)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(AccountSettingsActivity.this,
+                                            "User password is updated",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
 
+            }
+        });
+
+        //hide keyboard after typing email2
+        textViewUserEmail2.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (event != null&& (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
+                    InputMethodManager in =
+                            (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    in.hideSoftInputFromWindow(textViewUserEmail2.getApplicationWindowToken(),
+                            InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+                return false;
+            }
+        });
+        //hide keyboard after typing email2
+        textViewUserPassword2.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (event != null&& (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
+                    InputMethodManager in =
+                            (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    in.hideSoftInputFromWindow(textViewUserPassword2.getApplicationWindowToken(),
+                            InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+                return false;
             }
         });
 
@@ -183,21 +210,8 @@ public class AccountSettingsActivity extends AppCompatActivity {
                 Toast.makeText(AccountSettingsActivity.this,"Trying to find the user",Toast.LENGTH_SHORT).show();
                 for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     DataSnapshot email = ds.child("email");
-                    //Toast.makeText(AccountSettingsActivity.this,ds.getValue().toString()+" and "+user.getUid(),Toast.LENGTH_SHORT).show();
-                    //if(ds.getValue().toString().equals(user.getUid())){
                     if (email.getValue().toString().equals(user.getEmail())) {
-                        Toast.makeText(AccountSettingsActivity.this,"Found the user, "+ds.child("age").getValue().toString()+", "
-                                +ds.child("weight").getValue().toString()+", "+ds.child("firstname").getValue().toString()+", "
-                                +ds.child("lastname").getValue().toString(),Toast.LENGTH_SHORT).show();
-                        textViewUserAge.setText(ds.child("age").getValue().toString());
-                        textViewUserWeight.setText(ds.child("weight").getValue().toString());
-                        textViewUserFirstName.setText(ds.child("firstname").getValue().toString());
-                        textViewUserLastName.setText(ds.child("lastname").getValue().toString());
-
-                        userInfo.setFirstname(textViewUserFirstName.getText().toString());
-                        userInfo.setLastname(textViewUserLastName.getText().toString());
-                        userInfo.setAge(textViewUserAge.getText().toString());
-                        userInfo.setWeight(textViewUserWeight.getText().toString());
+                        //Toast.makeText(AccountSettingsActivity.this,"Found the user",Toast.LENGTH_SHORT).show();
                     }
                 }
             }
